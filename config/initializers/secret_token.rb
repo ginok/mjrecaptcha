ead04cc1928a393824efe1cca38d46b7805e8606
ginok <miragino@gmail.com>
2013-04-27 00:22:58 +0900

    Rails4の初期ファイルを追加

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mjrecaptcha::Application.config.secret_key_base = 'f67938a1f0fa12d315adf54dc0662fe7e0bd8b8ed9abc5df9c2ea4eeada087076ebbb74a01cc760ace45e88f2d7dc2f746023574de43c0ab8b3b3fd8e910b80a'
